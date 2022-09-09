66cd84a139c6ab249f3362a36bb55bedceac4174
jarenas98 <jarenas98@outlook.com>
2022-09-09 15:40:31 -0500

    config and eureka config

---
+++ service-registry/src/test/java/com/co/example/store/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.example.store.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
